ea04bb79a83920cec9b784d94ddf55d1c7054bb6
thinkySide <113565086+thinkySide@users.noreply.github.com>
2023-03-09 09:42:53 +0900

    Initial Commit

---
+++ Lab-Charts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab-Charts
+//
+//  Created by 김민준 on 2023/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
